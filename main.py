50feaf1081db97ec0fd917339352aed38d604f40
Дошан Есмаганбет <doshan.de@gmail.com>
2022-07-25 12:00:18 +0600

    create bot

---
+++ main.py
@@ -0,0 +1,28 @@
+# import discord
+# import asyncio
+from unicodedata import name
+from discord.ext import commands
+from config import TOKEN, PREFIX
+
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+# @bot.event
+# async def on_message(ctx):
+#     if ctx.content == 'exit':
+#         bot.close()
+#     elif ctx.author != bot.user:
+#         await ctx.reply(ctx.content)
+
+
+@bot.command(name="test")
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
+
+@bot.command(name="play")
+async def play(ctx, url):
+    await ctx.send(url)
+
+
+bot.run(TOKEN)
